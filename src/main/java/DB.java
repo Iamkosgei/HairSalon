8a1e6e3208e4054137da23076979650dd190a4e6
iamkosgei <kosgeik2930@gmail.com>
2019-04-21 21:43:16 +0300

    updated the password to the postgress database.

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,5 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do", "kosgei", "mrme2930");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "kosgei", "12345678");
 }
