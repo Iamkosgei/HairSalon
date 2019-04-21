30cd9129e55ff252f0cf326ae1ab82ef05f3fd30
iamkosgei <kosgeik2930@gmail.com>
2019-04-21 18:26:35 +0300

    Added methods to save clients to the database, find clients by their id and retrieve all clients.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,3 +1,7 @@
+import org.sql2o.Connection;
+
+import java.util.List;
+
 public class Client {
     private int id;
     private String first_name, last_name, phone, email, country , county;
@@ -43,5 +47,49 @@ public class Client {
 
     public int getStylistid() {
         return stylistid;
+    }
+
+    public static List<Client> all() {
+        String sql = "SELECT * FROM clients";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Client.class);
+        }
+    }
+
+    public void save() {
+        try(Connection con = DB.sql2o.open())  {
+            String sql = "INSERT INTO clients (first_name, last_name, phone, email, country, county,stylistid) VALUES (:first_name, :last_name, :phone, :email, :country, :county, :stylistid)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("first_name", this.first_name)
+                    .addParameter("last_name",this.last_name)
+                    .addParameter("phone",this.phone)
+                    .addParameter("email", this.email)
+                    .addParameter("country", this.country)
+                    .addParameter("county", this.county)
+                    .addParameter("stylistid", this.stylistid)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static Client find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM clients where id=:id";
+            Client client= con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Client.class);
+            return client;
+        }
+    }
+
+    @Override
+    public boolean equals(Object otherClient){
+        if (!(otherClient instanceof Client)) {
+            return false;
+        } else {
+            Client newClient = (Client) otherClient;
+            return this.email.equals(((Client) otherClient).email) &&
+                    this.getId() == newClient.id;
+        }
     }
 }
