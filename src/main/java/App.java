7aa647045d05906ef99138c2b39e2a917869449b
iamkosgei <kosgeik2930@gmail.com>
2019-04-21 20:46:43 +0300

    added a templates and routes to handle the various requests.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,60 @@
+import org.apache.log4j.BasicConfigurator;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
+
+        BasicConfigurator.configure();
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/index.vtl");
+            return new VelocityTemplateEngine().render(
+                    new ModelAndView(model, layout)
+            );
+        });
+
+        get("/stylists", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/stylists.vtl");
+            return new VelocityTemplateEngine().render(
+                    new ModelAndView(model, layout)
+            );
+        });
+
+        get("/stylists/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/add-stylist-form.vtl");
+            return new VelocityTemplateEngine().render(
+                    new ModelAndView(model, layout)
+            );
+        });
+
+
+
+        get("/clients", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/clients.vtl");
+            return new VelocityTemplateEngine().render(
+                    new ModelAndView(model, layout)
+            );
+        });
+        get("/clients/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/add-client-form.vtl");
+            return new VelocityTemplateEngine().render(
+                    new ModelAndView(model, layout)
+            );
+        });
 
     }
 }
