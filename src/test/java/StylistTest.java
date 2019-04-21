cd5799c62b9b26e4aa6652bf99dd18cb6244d643
iamkosgei <kosgeik2930@gmail.com>
2019-04-21 10:35:06 +0300

    added test to check whether stylist email,age and Id gets saved correctly

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -17,13 +17,30 @@ public class StylistTest {
     }
 
     @Test
-    public void newStylist_getsSecondName_john()
+    public void newStylist_getsSecondName_doe()
     {
         Stylist newStyList = new Stylist("john","doe" ,"johndoe@gmail.com",30,1);
         assertEquals("doe", newStyList.getSecondName());
     }
 
+    @Test
+    public void newStylist_getsEmail_johndoe()
+    {
+        Stylist newStyList = new Stylist("john","doe" ,"johndoe@gmail.com",30,1);
+        assertEquals("johndoe@gmail.com", newStyList.getEmail());
+    }
 
+    @Test
+    public void newStylist_getsAge_30()
+    {
+        Stylist newStyList = new Stylist("john","doe" ,"johndoe@gmail.com",30,1);
+        assertEquals(30, newStyList.getAge());
+    }
 
-
+    @Test
+    public void newStylist_getsId_1()
+    {
+        Stylist newStyList = new Stylist("john","doe" ,"johndoe@gmail.com",30,1);
+        assertEquals(1, newStyList.getId());
+    }
 }
