2944ef5b4db8d6fffd2bd80eda1f4b1c7d7f735e
iamkosgei <kosgeik2930@gmail.com>
2019-04-21 17:02:41 +0300

    added a setup and teardown method to setup and clear the table respectively

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,5 +1,24 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTest {
+    Client client;
+
+    @BeforeEach
+    public void setUp() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "kosgei", "12345678");
+        client = new Client("jane","doe","0723320981","janedoe@gmail.com","Kenya","Nairobi",1);
+    }
+    @AfterEach
+    public void tearDown() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM stylists *;";
+            con.createQuery(sql).executeUpdate();
+        }
+    }
 
 }
